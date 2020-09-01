f6ae7b7f268afa7d82f321109d2ee9a8097a9cce
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2020-09-01 14:43:53 +0300

    Added `pauseOnHover` option

--- src/MarqueeTextWidget.svelte
+++ src/MarqueeTextWidget.svelte
@@ -1,13 +1,24 @@
 <script>
   import { onMount } from "svelte";
+
   export let duration = 15;
   export let repeat = 2;
   export let paused = false;
+  export let pauseOnHover = false;
+
+  let hovered = false;
+
+  onMount(async () => {
+    const container = document.querySelector("#container");
+
+    container.onmouseover  = () => { hovered = true  }
+    container.onmouseleave = () => { hovered = false }
+  })
 </script>
 
 <style>
   .content {
-    width: 100000px;
+    width: 10000px;
   }
   .text {
     animation-name: animation;
@@ -15,22 +26,19 @@
     animation-iteration-count: infinite;
     float: left;
   }
-  .paused .text {
+  .paused {
     animation-play-state: paused;
   }
   @keyframes animation {
-    0% {
-      transform: translateX(0);
-    }
     100% {
       transform: translateX(-100%);
     }
   }
 </style>
 
-<div style="overflow: hidden;">
-  <div class="content" class:paused={paused === true}>
-    {#each Array(repeat) as _, i}
+<div style="overflow: hidden;" id="container">
+  <div class="content" class:paused={paused || (pauseOnHover && hovered)}>
+    {#each Array(repeat) as _}
       <div class="text" style="animation-duration: {duration}s">
         <slot />
       </div>
